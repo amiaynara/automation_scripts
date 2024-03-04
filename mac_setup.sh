cff462421822c9efa7e01af8cfb5fbc856c7556c
Amiay Narayan <amiayn@iitbhilai.ac.in>
2024-03-04 19:21:47 +0530

    Add automation setup file for macOS

---
+++ mac_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# install homebrew
+echo "Installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install oh my zsh! => to add some bling
+echo "Installing omzsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# to start vscode using `code .`
+echo "setting up VS CODE..."
+
+echo 'code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}' >> $HOME/.zshrc
+
+
+
+
+# add vim configuration
+
+
+
+
+
+# set python alias 
+
+
+
+# set up git
+echo "setting up ssh rsa key..."
+echo "creating SSH key"
+cd ~
+mkdir .ssh
+cd .ssh
+ssh-keygen -t rsa -b 4096 -C "amiayn@iitbhilai.ac.in"
+
+eval "$(ssh-agent -s)"	# with time you can make this more robust
+ssh-add --apple-use-keychain ~/.ssh/id_rsa
+
+touch config
+echo "Host github.com
+  AddKeysToAgent yes
+  UseKeychain yes 
+  IdentityFile ~/.ssh/id_
+" >> config
+
+
+
